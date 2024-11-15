e45a99755bfccf28983a80d689c84f84160bbe48
GuanQiuJian <2492311024@qq.com>
2024-11-15 17:38:54 +0800

    更新 finetune.py 文件

--- finetune.py
+++ finetune.py
@@ -1,15 +1,29 @@
 import os
+import json
 from typing import Optional, Dict, List, Tuple
 from dataclasses import dataclass, field
+from functools import partial
 
-import torch
-import transformers
-from transformers import AutoModel, AutoTokenizer
-
-from deepspeed import zero
+from deepspeed import zero # 我也不知道deepspeed有啥用啊，我就一张卡
 from deepspeed.runtime.zero.partition_parameters import ZeroParamStatus
 
+import torch
+from torchvision import transforms
+import transformers
+from transformers import AutoModel, AutoTokenizer, Trainer
+
+from peft import LoraConfig, get_peft_model, prepare_model_for_kbit_training
+from types import MethodType
 import logging
+import debugpy
+
+# 优雅的debug
+try:
+    debugpy.listen(("localhost", 9501))
+    print("Waiting for debugger attach")
+    debugpy.wait_for_client()
+except Exception as e:
+    pass
 
 @dataclass
 class ModelArguments:
@@ -78,6 +92,71 @@ def get_parameter_number(model):
         
     return {'Total': all_param, 'Trainable': trainable_params}
 
+# TODO:将图像转化成张量并归一化，这里也要按需修改
+def build_transform():
+    IMAGENET_INCEPTION_MEAN = (0.5, 0.5, 0.5) # timm.data.IMAGENET_INCEPTION_MEAN
+    IMAGENET_INCEPTION_STD = (0.5, 0.5, 0.5)  # timm.data.IMAGENET_INCEPTION_STD
+    return transforms.Compose(
+            [
+                transforms.ToTensor(),
+                transforms.Normalize(
+                    mean=IMAGENET_INCEPTION_MEAN, std=IMAGENET_INCEPTION_STD
+                ),
+            ]
+        )
+
+# TODO:数据处理模块，这里的参数是一定要改的
+def make_supervised_data_module(
+    tokenizer: transformers.PreTrainedTokenizer,
+    data_args,
+    transform,
+    data_collator=None,
+    llm_type="这里要自己定义",
+    slice_config=None,
+    patch_size=14,
+    query_nums=64,
+    batch_vision=False,
+    max_length=2048,
+) -> Dict:
+    dataset_cls = SupervisedDataset
+
+    rank0_print("Loading data...")
+
+    train_json = json.load(open(data_args.data_path, "r"))
+    train_dataset = dataset_cls(
+        train_json,
+        transform,
+        tokenizer,
+        slice_config=slice_config,
+        llm_type=llm_type,
+        patch_size=patch_size,
+        query_nums=query_nums,
+        batch_vision=batch_vision,
+        max_length=max_length,
+    )
+
+    if data_args.eval_data_path:
+        eval_json = json.load(open(data_args.eval_data_path, "r"))
+        eval_dataset = dataset_cls(
+            eval_json,
+            transform,
+            tokenizer,
+            slice_config=slice_config,
+            llm_type=llm_type,
+            patch_size=patch_size,
+            query_nums=query_nums,
+            batch_vision=batch_vision,
+            max_length=max_length,
+        )
+    else:
+        eval_dataset = None
+
+    return dict(
+        train_dataset=train_dataset,
+        eval_dataset=eval_dataset,
+        data_collator= partial(data_collator, max_length=max_length), #通过 partial 创建了一个新的函数，使max_length参数被固定为max_length。之后调用这个新的 data_collator函数不必再次提供 max_length
+    )
+
 def train():
     global local_rank
     parser = transformers.HfArgumentParser(
@@ -118,3 +197,106 @@ def train():
     tokenizer = AutoTokenizer.from_pretrained(
         model_args.model_name_or_path, trust_remote_code=True
     )
+
+    if not training_args.tune_vision:
+        model.vpm.requires_grad_(False)
+    if not training_args.tune_llm:
+        model.llm.requires_grad_(False)
+
+    if training_args.use_lora:
+        if training_args.use_lora and training_args.tune_llm:
+            raise ValueError("The model cannot simultaneously adjust LLM parameters and apply LoRA.")
+            
+        rank0_print("Currently using LoRA for fine-tuning the MiniCPM-V model.")
+        for name, param in model.llm.named_parameters():
+            # 冻结llm，这里也可以随机应变
+            param.requires_grad = False 
+        modules_to_save = ['embed_tokens','resampler']
+        if training_args.tune_vision:
+            modules_to_save.append('vpm')
+        # Lora配置，按需调整
+        lora_config = LoraConfig(
+            r=lora_args.lora_r,
+            lora_alpha=lora_args.lora_alpha,
+            target_modules=lora_args.lora_target_modules,
+            lora_dropout=lora_args.lora_dropout,
+            bias=lora_args.lora_bias,
+            layers_to_transform=lora_args.lora_layers_to_transform,
+            modules_to_save=modules_to_save,
+        )
+
+        # TODO:检查当前模型是否有get_input_embeddings方法，没有的话添加
+        # if not hasattr(model, 'get_input_embeddings'):
+        #     def get_input_embeddings(self):
+        #         return self.llm.get_input_embeddings()
+        #     model.get_input_embeddings = MethodType(get_input_embeddings, model)
+
+        # TODO:QLora需要的话在这里写
+        # if lora_args.q_lora:
+        #     model = prepare_model_for_kbit_training(
+        #         model, use_gradient_checkpointing=training_args.gradient_checkpointing
+        #     )
+        
+        model = get_peft_model(model, lora_config)
+
+        # 是否使用梯度检查点，时间换空间，建议使用
+        if training_args.gradient_checkpointing:
+            model.enable_input_require_grads()
+
+    rank0_print("Model parameters: ", get_parameter_number(model))
+
+    llm_type = training_args.llm_type
+    
+    rank0_print(f"llm_type={llm_type}")
+
+    # TODO:考虑一下要不要分片，MiniCPM-V是8B模型做了分片，我需不需要
+    if hasattr(model.config, "slice_config"):
+        model.config.slice_config.max_slice_nums = training_args.max_slice_nums
+        slice_config = model.config.slice_config.to_dict()
+    else:
+        model.config.max_slice_nums = training_args.max_slice_nums
+        slice_config = model.config.to_dict()
+    
+    # TODO:批量视觉输入？我真的需要这个东西吗？
+    if hasattr(model.config, "batch_vision_input"):
+        batch_vision = model.config.batch_vision_input
+    else:
+        batch_vision = False
+    
+    # 图像预处理-这不是分片操作
+    transform_func = build_transform()
+    # TODO:这里要自己定义，根据数据集不同，参数的要求也不同
+    data_module = make_supervised_data_module(
+        tokenizer=tokenizer,
+        data_args=data_args,
+        transform=transform_func,
+        data_collator=data_collator,
+        slice_config=slice_config,
+        llm_type=llm_type,
+        patch_size=model.config.patch_size,
+        query_nums=model.config.query_num,
+        batch_vision=batch_vision,
+        max_length=training_args.model_max_length,
+    )
+    
+    # TODO:这里真的只需要继承别人的Trainer吗？需要重写Trainer的一些方法吗？
+    trainer = Trainer(
+        model=model,
+        tokenizer=tokenizer,
+        args=training_args,
+        **data_module,
+    )
+    
+    trainer.train()
+    trainer.save_state()
+    
+    safe_save_model_for_hf_trainer(
+        trainer=trainer,
+        output_dir=training_args.output_dir,
+        bias=lora_args.lora_bias)
+    
+
+if __name__ == "__main__":
+    train()
+
+    
